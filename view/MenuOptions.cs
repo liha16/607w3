2358c5be4fa8e2140997a1f3e71707d45e9764c8
Lisa Veltman Hellinger <lv222fz@student.lnu.se>
2020-10-21 13:29:51 +0200

    your msg

---
+++ view/MenuOptions.cs
@@ -0,0 +1,12 @@
+namespace BlackJack.view
+{       
+    public enum MenuOptions
+    {
+        Play = 'p',
+        Hit = 'h',
+        Stand = 's',
+        Quit = 'q'
+    }
+}
+
+//'p' to Play, 'h' to Hit, 's' to Stand or 'q' to Quit
---
+++ view/SimpleView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.view
+{
+    class SimpleView : IView
+    {
+        public MenuOptions MenuOptions{get;} // Menu options
+
+        public void DisplayWelcomeMessage()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Hello Black Jack World");
+            System.Console.WriteLine("Type 'p' to Play, 'h' to Hit, 's' to Stand or 'q' to Quit\n");
+            //DisplayMenuOptions();
+            
+        }
+
+        private void DisplayMenuOptions ()
+        {
+            string[] names = Enum.GetNames(typeof(MenuOptions));
+            MenuOptions[] values = (MenuOptions[])Enum.GetValues(typeof(MenuOptions));
+
+            for( int i = 0; i < names.Length; i++ )
+            {
+                System.Console.WriteLine(names[i], values[i]);
+            }
+        }
+        
+        public int GetInput()
+        {
+            return System.Console.In.Read();
+        }
+
+        public void DisplayCard(model.Card a_card)
+        {
+            System.Console.WriteLine("{0} of {1}", a_card.GetValue(), a_card.GetColor());
+        }
+
+        public void DisplayPlayerHand(IEnumerable<model.Card> a_hand, int a_score)
+        {
+            DisplayHand("Player", a_hand, a_score);
+        }
+
+        public void DisplayDealerHand(IEnumerable<model.Card> a_hand, int a_score)
+        {
+            DisplayHand("Dealer", a_hand, a_score);
+        }
+
+        private void DisplayHand(String a_name, IEnumerable<model.Card> a_hand, int a_score)
+        {
+            System.Console.WriteLine("{0} Has: ", a_name);
+            foreach (model.Card c in a_hand)
+            {
+                DisplayCard(c);
+            }
+            System.Console.WriteLine("Score: {0}", a_score);
+            System.Console.WriteLine("");
+        }
+
+        public void DisplayGameOver(bool a_dealerIsWinner)
+        {
+            System.Console.Write("GameOver: ");
+            if (a_dealerIsWinner)
+            {
+                System.Console.WriteLine("Dealer Won!");
+            }
+            else
+            {
+                System.Console.WriteLine("You Won!");
+            }
+            
+        }
+    }
+}
